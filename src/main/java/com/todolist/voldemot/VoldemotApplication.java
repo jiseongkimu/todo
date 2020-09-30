516dc9125c0ac31e12f2e950c65311dfd7bd7de0
jiseongkimu <jiseongkimu@gmail.com>
2020-09-30 10:56:17 +0900

    Initial commit

---
+++ src/main/java/com/todolist/voldemot/VoldemotApplication.java
@@ -0,0 +1,13 @@
+package com.todolist.voldemot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoldemotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoldemotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todolist/voldemot/VoldemotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist.voldemot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoldemotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
